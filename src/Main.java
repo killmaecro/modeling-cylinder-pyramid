8928388c4b5e6ff587d312f02dc8e355d9b56f5e
Joshua <josh1macp@gmail.com>
2024-09-25 17:25:20 -0400

    removed loop and scanner from main

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,6 @@
  * Description: Program that, when given the height and radius of a cylinder,
  * calculates a cylinder pyramid's total volume.
  **/
-import java.util.Scanner;
 public class Main {
 
 	// Main entry point to the program
@@ -15,11 +14,6 @@ public class Main {
 		// Creating a new pyramid object
 		ModelingCylinder pyramid = new ModelingCylinder();
 
-		Scanner reader = new Scanner(System.in);
-
-		boolean satisfied = false;
-
-		while (!satisfied) {
 			// Ask user for the radius of the cylinder
 			pyramid.setRadius(GetInput.getArgs("Enter the radius of the cylinder: "));
 			System.out.println("You entered: " + pyramid.getRadius());
@@ -28,17 +22,8 @@ public class Main {
 			pyramid.setHeight(GetInput.getArgs("Enter the height of the cylinder: "));
 			System.out.println("You entered: " + pyramid.getHeight());
 
-			System.out.println("Are you satisfied with these numbers? Type yes if so.");
-			String answer = reader.next();
-
-			if (answer.equalsIgnoreCase("yes")) {
-				satisfied = true;
-			}
-		}
-
-		reader.close();
-
 		// Displaying result
 		pyramid.displayResult();
 	}
 }
+
