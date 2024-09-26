43c05f2113b46452a06d209db2aaeed117710d11
Joshua <josh1macp@gmail.com>
2024-09-26 17:37:53 -0400

    removed getInput class for assignment, printf for result is now in Main like the project asks

--- src/Main.java
+++ src/Main.java
@@ -6,24 +6,29 @@
  * Description: Program that, when given the height and radius of a cylinder,
  * calculates a cylinder pyramid's total volume.
  **/
+import java.util.Scanner;
 public class Main {
-
 	// Main entry point to the program
 	public static void main(String[] args) {
 
+		Scanner kbd = new Scanner(System.in);
+
 		// Creating a new pyramid object
-		ModelingCylinder pyramid = new ModelingCylinder();
+		ModelingPyramid pyramid = new ModelingPyramid();
 
 			// Ask user for the radius of the cylinder
-			pyramid.setRadius(GetInput.getArgs("Enter the radius of the cylinder: "));
+			System.out.println("Enter the radius of the cylinder: ");
+			pyramid.setRadius(kbd.nextDouble());
 			System.out.println("You entered: " + pyramid.getRadius());
 
 			// Ask user for the height of the cylinder
-			pyramid.setHeight(GetInput.getArgs("Enter the height of the cylinder: "));
+			System.out.println("Enter the height of the cylinder: ");
+			pyramid.setHeight(kbd.nextDouble());
 			System.out.println("You entered: " + pyramid.getHeight());
 
 		// Displaying result
-		pyramid.displayResult();
+		System.out.printf("The total volume of the cylinder pyramid is %.3f units cubed. \n%s"
+				, pyramid.calculateVolume(), "Program by: Joshua MacPherson");
 	}
 }
 
--- src/ModelingPyramid.java
+++ src/ModelingPyramid.java
@@ -1,13 +1,13 @@
 /**
  * Description: This class takes care of all the calculations for the cylinder pyramid.
  */
-public class ModelingCylinder {
+public class ModelingPyramid {
 
 	private double radius;
 	private double height;
 
-    // Constructor method
-	public ModelingCylinder() {
+	// Constructor method
+	public ModelingPyramid() {
 
 	}
 
@@ -38,16 +38,11 @@ public class ModelingCylinder {
 	 * Volume Calculator calculator.net
 	 * https://www.calculator.net/volume-calculator.html (Accessed September 12, 2024)
 	 */
-	public double calculateVolume(double radius, double height) {
+	public double calculateVolume() {
 		double volume = height * Math.pow(radius, 2) * Math.PI;
-        volume += height * Math.pow(radius * .8, 2) * Math.PI;
+		volume += height * Math.pow(radius * .8, 2) * Math.PI;
 		volume += height * Math.pow(radius * .64, 2) * Math.PI;
 		return volume;
 	}
 
-	// Method to display the result
-	public void displayResult() {
-		System.out.printf("The total volume of the cylinder pyramid is %.3f units cubed. \n%s"
-				, calculateVolume(radius, height), "Program by: Joshua MacPherson");
-	}
 }
